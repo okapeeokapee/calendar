c8de93cd215300e0b39a27e3559f4d8b7877df66
okapeeokapee <okapee420@gmail.com>
2020-09-29 18:15:41 +0900

    make dates[];

--- ja/main.js
+++ ja/main.js
@@ -1,4 +1,15 @@
 'use strict';{
   const year = 2020;
   const month = 4;
+
+  function getCalendarBody(){
+    const dates = [];
+    const lastDates = new Date (year, month + 1, 0).getDate();
+
+    for(let i = 1; i <= lastDates; i++){
+      dates.push(i);
+    }
+    console.log(dates);
+  }
+  getCalendarBody();
 }
