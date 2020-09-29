2cc9f2b7d6369e2a2a748aa4bd2ae44be1eb4df0
okapeeokapee <okapee420@gmail.com>
2020-09-29 22:43:11 +0900

    calendar head add

--- ja/main.js
+++ ja/main.js
@@ -1,15 +1,39 @@
-'use strict';{
+'use strict';
+
+console.clear(); 
+
+{
   const year = 2020;
-  const month = 4;
+  const month = 4;//5月
+
+  function getCalendarHead(){
+    const dates = [];
+    const d = new Date(year, month, 0).getDate();
+    const n = new Date(year, month, 1).getDay();
+    for(let i = 0; i < n; i++){
+      dates.unshift({
+        date:d - i,
+        isToday:false,
+        isDisabled:true,
+      });
+    }
+
+    console.log(dates);
+  }
 
   function getCalendarBody(){
     const dates = [];
     const lastDates = new Date (year, month + 1, 0).getDate();
 
     for(let i = 1; i <= lastDates; i++){
-      dates.push(i);
+      dates.push({
+        date:i,
+        isToday:false,
+        isDisabled:false,
+      });
     }
     console.log(dates);
   }
-  getCalendarBody();
+  getCalendarHead();
+  // getCalendarBody();
 }
